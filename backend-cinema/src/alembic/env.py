b32cc43c042d9266421bb7ec15e61d48b01ddd1b
omega050 <mauro-jr50@hotmail.com>
2025-07-22 16:10:53 -0300

    fix: Alembic autogenerate migration

--- backend-cinema/src/alembic/env.py
+++ backend-cinema/src/alembic/env.py
@@ -30,8 +30,6 @@ config = context.config
 
 config.set_main_option("sqlalchemy.url", db_url)
 
-target_metadata = Base.metadata
-
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -40,7 +38,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
